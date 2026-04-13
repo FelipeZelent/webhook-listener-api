033ccb2d87ef5b87385c123db2c38c52a167c169
FelipeZelent <felipezelent@gmail.com>
2026-04-13 17:54:59 -0300

    v0.0.1

---
+++ src/main/java/com/felipe/webhook_listener_api/WebhookListenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.webhook_listener_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookListenerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookListenerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/webhook_listener_api/WebhookListenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.webhook_listener_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookListenerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
